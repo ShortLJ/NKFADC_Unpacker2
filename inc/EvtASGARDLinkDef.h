7553f985061ae241e7774461842525dd70836ed8
Jaehwan Lee <eaw0224@korea.ac.kr>
2025-07-03 17:00:34 +0900

    started cmake

---
+++ inc/EvtASGARDLinkDef.h
@@ -0,0 +1,17 @@
+
+#ifdef __CLING__
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#include "Sig.h"
+#include "SigAna.h"
+#pragma link C++ class SigAna+;
+#pragma link C++ class vector<SigAna>+;
+#include "HitCrystal.h"
+#pragma link C++ class HitCrystal+;
+#pragma link C++ class vector<HitCrystal>+;
+#include "HitClover.h"
+#pragma link C++ class HitClover+;
+#pragma link C++ class vector<HitClover>+;
+#include "EvtASGARD.h"
+#pragma link C++ class EvtASGARD+;
+#endif
---
+++ inc/EvtStarkJrLinkDef.h
@@ -0,0 +1,18 @@
+
+#ifdef __CLING__
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#include "Sig.h"
+#include "SigAna.h"
+#pragma link C++ class SigAna+;
+#pragma link C++ class vector<SigAna>+;
+#include "HitX6.h"
+#pragma link C++ class HitPad+;
+#pragma link C++ class vector<HitPad>+;
+#pragma link C++ class HitStrip+;
+#pragma link C++ class vector<HitStrip>+;
+#pragma link C++ class HitX6+;
+#pragma link C++ class vector<HitX6>+;
+#include "EvtStarkJr.h"
+#pragma link C++ class EvtStarkJr+;
+#endif
